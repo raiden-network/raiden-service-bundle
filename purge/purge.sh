1fa6ea55ee7113f375a0fe30eb136e4be7261544
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2018-10-18 15:13:22 +0200

    Add deployment configuration

--- purge/purge.sh
+++ purge/purge.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
+set -e
+
+echo "======================="
+echo -n "Started at: " ; date
+
 if [ ! -d ./venv ]; then
     python3 -m venv venv
     . ./venv/bin/activate
     pip3 install -r requirements.txt
 else
     . ./venv/bin/activate
+fi
+
+# if PURGE_SLEEP_UNTIL env is defined and is a time in the format "%H:%m[:%s]",
+# sleep until then before continuing
+if [ -n $PURGE_SLEEP_UNTIL ]; then
+    SLEEP_FOR=$(( ( ( $(date -d "$PURGE_SLEEP_UNTIL" '+%s') - $(date '+%s') ) + 86400 ) % 86400 ))
+    echo -n "Sleeping until: " ; date -d "+$SLEEP_FOR seconds"
+    sleep $SLEEP_FOR
 fi
 
 exec python3 purge.py "$@"
