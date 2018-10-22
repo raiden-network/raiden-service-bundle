1e499084ea534495f3c02121a25dc3723c24132b
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2018-10-22 17:41:56 +0200

    Expose URL list variable to compose

--- build/synapse/synapse-entrypoint.sh
+++ build/synapse/synapse-entrypoint.sh
@@ -4,6 +4,6 @@
 mkdir -p /data/log
 mkdir -p /data/keys
 
-python3 /bin/render_config_template.py
+/synapse-venv/bin/python /bin/render_config_template.py
 /synapse-venv/bin/python -m synapse.app.homeserver --config-path /config/synapse.yaml --generate-keys
-/synapse-venv/bin/python -m synapse.app.homeserver --config-path /config/synapse.yaml
+exec /synapse-venv/bin/python -m synapse.app.homeserver --config-path /config/synapse.yaml
