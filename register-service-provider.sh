9d2dfdc5d712d31a4232511e4f30f3b768941a4b
Oliver Bünting <oliver@brainbot.com>
2020-03-31 10:10:40 +0200

    changing registered service url
    According to @karlb the raiden clients only accept a Service URL that uses `https://pfs.${SERVER_NAME}`

--- register-service-provider.sh
+++ register-service-provider.sh
@@ -11,7 +11,7 @@ docker run --rm -it \
   -e SR_REGISTER_LOG_LEVEL=${LOG_LEVEL} \
   -e SR_REGISTER_KEYSTORE_FILE=/keystore/${KEYSTORE_FILE} \
   -e SR_REGISTER_PASSWORD=${PASSWORD} \
-  -e SR_REGISTER_SERVICE_URL=pfs.${SERVER_NAME} \
+  -e SR_REGISTER_SERVICE_URL=https://pfs.${SERVER_NAME} \
   -e SR_REGISTER_ETH_RPC=${ETH_RPC} \
   --env-file .env \
   ${IMAGE} \
