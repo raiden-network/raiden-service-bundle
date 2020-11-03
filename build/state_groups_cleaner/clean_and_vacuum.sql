7822be0ba3c786bc2a068132fa9303d2c245b231
Konrad Feldmeier <konrad@brainbot.com>
2020-11-03 16:31:36 +0100

    [state_groups_cleaner] Initial commit

---
+++ build/state_groups_cleaner/clean_and_vacuum.sql
@@ -0,0 +1,6 @@
+CREATE TEMPORARY TABLE unreffed(id BIGINT PRIMARY KEY);
+COPY unreffed FROM '/var/lib/postgresql/data/sgs.csv' WITH (FORMAT 'csv');  
+
+DELETE FROM state_groups_state WHERE state_group IN (SELECT id FROM unreffed);
+DELETE FROM state_group_edges WHERE state_group IN (SELECT id FROM unreffed);
+DELETE FROM state_groups WHERE id IN (SELECT id FROM unreffed);
