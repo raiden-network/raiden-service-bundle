9bd3b171c28f6330312ad7a9f89588677cbe7200
Konrad Feldmeier <konrad@brainbot.com>
2020-11-03 16:51:04 +0100

    [state_groups_cleaner] Use curl socket docker API
    We query all containers and filter for image names containing `synapse`.
    Those containers are stopped.
    Stop status of containers is queried in a loop.
    After running the cleaning, containers are started again.
    
    This also applies a couple shellcheck suggested fixes to the script.

--- build/state_groups_cleaner/state_group_cleaner.sh
+++ build/state_groups_cleaner/state_group_cleaner.sh
@@ -1,29 +1,62 @@
 #!/usr/bin/env bash
 
+## Global parameters
+WAIT_TO_KILL_SYNAPSE=300
+REGULAR_SLEEP=$((3600 * 7 * 24))
+
 # Select a random start time within the next ~7 days
-INITIAL_SLEEP = $(( RANDOM * 2.63 * 7 ))
-sleep $INITIAL_SLEEP
+INITIAL_SLEEP=$(echo "$(( RANDOM )) * 2.63 * 7" | bc)
+sleep "$INITIAL_SLEEP"
 
 while true;
 do
-    # TODO: stop synapse containers
-    # SYNAPSE_CONTAINERS=$(docker ps|grep synapse|cut -d' ' -f1)
-    # docker stop -t 600 $SYNAPSE_CONTAINERS
+    # Find all running containers with an image name containing "synapse"
+    SYNAPSE_CONTAINERS=$(curl -Ssg --unix-socket /var/run/docker.sock \
+        -XGET 'http://docker/containers/json?filters={"status":["running"]}' |\
+        jq -r '.[] | select(.Image | contains("synapse"))|.Id')
 
+    # Stop the synapse containers (in background)
+    for container_id in $SYNAPSE_CONTAINERS;
+    do
+        curl -Ssg --unix-socket /var/run/docker.sock -XPOST \
+        "http://docker/containers/$container_id/stop?t=$WAIT_TO_KILL_SYNAPSE" &
+    done
+
+    # Stop calls were detached, we probe the API to see when we're done
+    SYNAPSE_RUNNING=$SYNAPSE_CONTAINERS
+    while [[ $SYNAPSE_RUNNING ]];
+    do
+        sleep 1
+        SYNAPSE_RUNNING=$(curl -Ssg --unix-socket /var/run/docker.sock \
+            -XGET 'http://docker/containers/json?filters={"status":["running"]}' |\
+            jq -r '.[] | select(.Image | contains("synapse"))|.Id')
+    done
+    echo "All synapses down"
+
+    # Construct a filename
     FN=/data/sgs_$(date -Iseconds).csv
 
-    rust-synapse-find-unreferenced-state-groups -p postgres://postgres@db/synapse -o $FN 
+    rust-synapse-find-unreferenced-state-groups -p postgres://postgres@db/synapse -o "$FN"
 
     touch /data/sgs.csv
-    rm /data/sgs.csv 
-    cp $FN /data/sgs.csv
+    rm /data/sgs.csv
+    cp "$FN" /data/sgs.csv
+
+    date
 
     # now remotely run psql to delete and vacuum
     psql -u postgres -h db -w -d synapse -f clean_and_vacuum.sql
 
-    # TODO: restart synapse containers
-    # docker start $SYNAPSE_CONTAINERS
+    date
 
-    # Now sleep for another week
-    sleep $((3600 * 24 * 7))
+    # restart synapse containers
+    for container_id in $SYNAPSE_CONTAINERS;
+    do
+        curl -Ssg --unix-socket /var/run/docker.sock -XPOST \
+        "http://docker/containers/$container_id/start"
+
+    done
+
+    # Now sleep until next execution
+    sleep $REGULAR_SLEEP
 done
