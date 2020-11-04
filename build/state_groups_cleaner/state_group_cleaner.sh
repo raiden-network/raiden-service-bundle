2b7084fbca3c7c5e06788d4aebcb2f19e0d4f7c3
Konrad Feldmeier <konrad@brainbot.com>
2020-11-04 18:01:40 +0100

    [state_groups_cleaner] Fix a comment line
    Also add some more logging.

--- build/state_groups_cleaner/state_group_cleaner.sh
+++ build/state_groups_cleaner/state_group_cleaner.sh
@@ -6,20 +6,23 @@ REGULAR_SLEEP=$((3600 * 7 * 24))
 
 # Select a random start time within the next ~7 days
 INITIAL_SLEEP=$(echo "$(( RANDOM )) * 2.63 * 7" | bc)
+date
 echo "Sleeping for $INITIAL_SLEEP seconds"
 sleep "$INITIAL_SLEEP"
 
 while true;
 do
-    # Find all running containers with an image name containing "synapse"
+    date
+    # Find all running containers that have a stop label for state_group cleaning
     SYNAPSE_CONTAINERS=$(curl -Ssg --unix-socket /var/run/docker.sock \
         -XGET 'http://docker/containers/json?filters={"status":["running"],"label":["state_groups_cleaner_stop=true"]}' |\
         jq -r '.[].Id')
 
+    CONTAINER_COUNT=$(echo $SYNAPSE_CONTAINERS | wc -w)
+    echo "Stopping $CONTAINER_COUNT containers with label 'state_groups_cleaner_stop=true'..."
     # Stop the synapse containers (in background)
     for container_id in $SYNAPSE_CONTAINERS;
     do
-        echo "Stopping all containers with label 'state_groups_cleaner_stop=true'"
         curl -Ssg --unix-socket /var/run/docker.sock -XPOST \
         "http://docker/containers/$container_id/stop?t=$WAIT_TO_KILL_SYNAPSE" &
     done
@@ -33,7 +36,7 @@ do
             -XGET 'http://docker/containers/json?filters={"status":["running"],"label":["state_groups_cleaner_stop=true"]}' |\
             jq -r '.[].Id')
     done
-    echo "All synapses down"
+    echo "All synapses down."
 
     # Construct a filename
     FN=/data/sgs_$(date -Iseconds).csv
