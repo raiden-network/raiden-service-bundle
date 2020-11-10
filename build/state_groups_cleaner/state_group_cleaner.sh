dd291fbb04e81ed1bc7100924b389e3b08f7fd05
Konrad Feldmeier <konrad@brainbot.com>
2020-11-10 15:20:18 +0100

    [state_groups_cleaner] Prevent early restarts
    This adds a check for the last run. If the last run was less than
    the configured interval (currently one week) ago, the script will
    add the necessary wait time.
    
    This allows to have the container always restarted by docker-compose.

--- build/state_groups_cleaner/state_group_cleaner.sh
+++ build/state_groups_cleaner/state_group_cleaner.sh
@@ -4,10 +4,27 @@ set -eo pipefail
 
 ## Global parameters
 WAIT_TO_KILL_SYNAPSE=120  # most likely useless, it seems synapse doesn't stop without being forced
-REGULAR_SLEEP=$((3600 * 7 * 24))
+REGULAR_SLEEP=$((3600 * 24 * 7))
 
-# Select a random start time within the next ~7 days
-INITIAL_SLEEP=$(echo "$(( RANDOM )) * 2.63 * 7" | bc)
+# Check for last run (if any)
+LAST_FN="$(ls /data/sgs_*.csv|cut -d'_' -f2|cut -d'.' -f1|tail -1)"
+
+if [[ $LAST_FN ]];
+then
+    LAST_RUN_UTC=$(date --date="$LAST_FN" "+%s")
+    TIME_SINCE_LAST_RUN=$(echo "$(date '+%s') - $LAST_RUN_UTC" | bc)
+    EARLY_RESTART=$(echo "$TIME_SINCE_LAST_RUN < $REGULAR_SLEEP" | bc)
+fi
+
+if [[ $EARLY_RESTART -gt 0 ]];
+then
+    # Sleep for the remainder of one week
+    INITIAL_SLEEP=$(echo "$REGULAR_SLEEP - $TIME_SINCE_LAST_RUN" | bc)
+else
+    # Select a random start time within the next ~7 days
+    INITIAL_SLEEP=$(echo "$(( RANDOM )) * 2.63 * 7" | bc)
+fi
+
 date
 echo "Sleeping for $INITIAL_SLEEP seconds"
 sleep "$INITIAL_SLEEP"
